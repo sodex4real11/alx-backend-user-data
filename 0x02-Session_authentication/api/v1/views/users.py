bec77fd89214a7de066baeb776c49d5cccf7fc13
sodex4real11 <sodex4real11@gmail.com>
2024-06-06 01:45:45 +0000

    Sessions

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from typing import TypeVar
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
@@ -18,18 +19,28 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
+    """ GET /api/v1/users/id or me
     Path parameter:
       - User ID
+      - Me user
     Return:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
         abort(404)
+
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+
+    user: TypeVar('User') = User()
+    if user_id == 'me' and request.current_user is not None:
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
+
     return jsonify(user.to_json())
 
 
